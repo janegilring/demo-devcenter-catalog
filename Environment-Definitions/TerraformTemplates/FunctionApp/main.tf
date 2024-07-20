f785c688392bc067d389a604c03deab2b43ec6b4
Jan Egil Ring <janegilring@microsoft.com>
2024-07-20 20:37:33 +0200

    Changed to workspace-mode
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- Environment-Definitions/TerraformTemplates/FunctionApp/main.tf
+++ Environment-Definitions/TerraformTemplates/FunctionApp/main.tf
@@ -39,11 +39,20 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "func-la"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 resource "azurerm_application_insights" "main" {
   name                = replace(var.resource_name, " ", "-")
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = var.location
   application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.main.id
   tags                = {}
 }
 
